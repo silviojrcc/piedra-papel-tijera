e42b92d1060cb9d41ed49c94112d32679eb7f655
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-04-17 23:16:20 -0300

    Agrega estilos a los botones

--- components/custom-button/index.js
+++ components/custom-button/index.js
@@ -13,7 +13,34 @@ function initCustomButton(){
             button.textContent= this.textContent;
             button.className = "boton";
 
-            shadow.appendChild(button);
+            style.innerHTML = `
+            .boton{
+                width: 322px;
+                background-color: #006CFC;
+                color:#fff;
+                font-size:35px;
+                border: solid 4px #001997;
+                border-radius: 15px;
+                margin:0;
+                padding:20px 0;
+                font-family: 'Odibee Sans', cursive;
+            }
+
+            .boton:hover{
+                cursor: pointer;
+                background-color: #0d5bc1;
+            }
+
+            @media (min-width: 769px){
+                .boton{
+                    width: 404px;
+                    font-size:45px;
+                }
+              }
+        `;
+
+        shadow.appendChild(button);
+        shadow.appendChild(style);
 
         }
     }

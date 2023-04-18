219a47783aa3669573aa4eb87559c213890af7ff
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-04-17 23:40:32 -0300

    Agrega score del juego

---
+++ components/score/index.js
@@ -0,0 +1,48 @@
+function initGameScore() {
+    class GameScore extends HTMLElement {
+      constructor() {
+        super();
+        this.render();
+      }
+      render() {
+        const shadow = this.attachShadow({ mode: "open" });
+        const div = document.createElement("div");
+        const style = document.createElement("style");
+        const score = state.getScore();
+  
+        div.innerHTML = `
+        <div class="container">
+          <h3 class="title">Score</h3>
+          <p class="text">Player: ${score.player}</p>
+          <p class="text">Maquina: ${score.computer}</p>
+        </div>
+        `;
+  
+        style.innerHTML = `
+          .container{
+              border:solid black 5px;
+              border-radius:2px;
+              min-width:230px;
+              min-height:190px;
+              background-color:white;
+          }
+  
+          .title{
+              font-size:50px;
+              text-align:center;
+              color:black;
+          }
+          
+          .text{
+              font-size:35px;
+              text-align:right;
+              padding:5px;
+          }
+        `;
+  
+        shadow.appendChild(div);
+        shadow.appendChild(style);
+      }
+    }
+    customElements.define("game-score", GameScore);
+  }

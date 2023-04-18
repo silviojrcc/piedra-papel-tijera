3a3d89744e53f3a9124be78bf49b1fe3df8dfa41
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-04-18 00:01:57 -0300

    Se agrega ruta para la pagina welcome

---
+++ router.js
@@ -0,0 +1,32 @@
+const routes = [
+    { path: /\/piedra-papel-tijera\/welcome/, component: initWelcomePage },
+  ];
+  
+  function initRouter(container) {
+    function goTo(path) {
+      history.pushState({}, "", path);
+      handleRoute(path);
+    }
+    
+    function handleRoute(route) {
+      for (const r of routes) {
+        if (r.path.test(route)) {
+          const el = r.component({ goTo: goTo });
+          if (container.firstChild) {
+            container.firstChild.remove();
+          }
+          container.appendChild(el);
+        }
+      }
+    }
+  
+    if (location.host.includes("netlify.app") || "/") {
+      goTo("/piedra-papel-tijera/welcome");
+    } else {
+      handleRoute(location.pathname);
+    }
+    
+    window.onpopstate = function () {
+      handleRoute(location.pathname);
+    };
+  }

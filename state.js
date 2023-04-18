830a3453941970c2457cab836a854e387ea8e6b5
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-04-17 23:08:08 -0300

    agrega las propiedades del estado

---
+++ state.js
@@ -0,0 +1,16 @@
+const state = {
+    data: {
+      currentGame: {
+        computerPlay: "",
+        playerPlay: "",
+        result: ""
+      },
+      playHistory: {
+        player: 0,
+        computer: 0,
+      },
+    },
+  
+  };
+  
+  

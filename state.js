904c6ca15058738cd9f52411f4f19f42677f28e0
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-04-17 23:09:37 -0300

    Agrega setters y getters del estado

--- state.js
+++ state.js
@@ -1,16 +1,26 @@
 const state = {
-    data: {
-      currentGame: {
-        computerPlay: "",
-        playerPlay: "",
-        result: ""
-      },
-      playHistory: {
-        player: 0,
-        computer: 0,
-      },
+  data: {
+    currentGame: {
+      computerPlay: "",
+      playerPlay: "",
+      result: "",
     },
-  
-  };
-  
-  
+    playHistory: {
+      player: 0,
+      computer: 0,
+    },
+  },
+
+  getScore() {
+    return this.data.playHistory;
+  },
+
+  getState() {
+    return this.data;
+  },
+
+  setState(newState) {
+    this.data = newState;
+    localStorage.setItem("saved-state", JSON.stringify(newState));
+  },
+};

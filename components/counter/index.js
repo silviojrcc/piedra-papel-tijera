37389b86e84c3d12d62367c3ffeefc39223ec8b6
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-04-17 23:45:50 -0300

    Agrega logica contador del juego

---
+++ components/counter/index.js
@@ -0,0 +1,40 @@
+function initCounter() {
+    class Counter extends HTMLElement {
+      constructor() {
+        super();
+        this.render();
+      }
+      render() {
+        let counter = 3;
+        const shadow = this.attachShadow({ mode: "open" });
+        const div = document.createElement("div");
+        const style = document.createElement("style");
+        style.innerHTML = `
+          .counter{
+              color: black;
+              min-width:60px;
+              min-height:80px;
+              font-size:100px;
+          }        
+        `;
+        const intervalo = setInterval(() => {
+          div.innerHTML = `
+              <div class="counter">${counter}</div>
+            `;
+          if (counter <= 0) {
+            clearInterval(intervalo);
+            const contador = new CustomEvent("change", {
+              detail: {
+                numero: counter,
+              },
+            });
+            this.dispatchEvent(contador);
+          }
+          counter--;
+        }, 1000);
+        shadow.appendChild(div);
+        shadow.appendChild(style);
+      }
+    }
+    customElements.define("custom-counter", Counter);
+  }
